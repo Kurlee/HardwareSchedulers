2f7581d486a6c260bf34fb192afdd5289236eb86
kurlee <adrianabdala1989@gmail.com>
2019-07-01 20:37:28 -0400

    finished disk scheduler

--- disk.c
+++ disk.c
@@ -84,7 +84,7 @@ struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int
 	  int dist_cylinder; // distance from the requested cylinder to the current cylinder
 	  int is_closest; // marked as one of the rcb's requesting the next closest cylinder
   };
-  struct REL_REQ rr[*queue_cnt]; 
+  struct REL_REQ rr[*queue_cnt];
 
   int cr; // closest request made by any rcb
   // calculates the distance to the next cylinder for each rcb
@@ -93,9 +93,9 @@ struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int
   {
 	  rr[i].rcb = request_queue[i];
 	  rr[i].is_closest = 0;
-	  // This is kinda hackey. the online compiler does not include math.h. 
-	  // so this will effectively be distance squared, but it shouldnt matter 
-	  // larger numbers mean farther away, smaller means closer. 
+	  // This is kinda hackey. the online compiler does not include math.h.
+	  // so this will effectively be distance squared, but it shouldnt matter
+	  // larger numbers mean farther away, smaller means closer.
 	  rr[i].dist_cylinder = (request_queue[i].cylinder - current_cylinder) * (request_queue[i].cylinder - current_cylinder);
 	  if (i == 0)
 		  cr = rr[i].dist_cylinder;
@@ -123,21 +123,19 @@ struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int
 	  {
 		  nextrcb = rr[i].rcb;
 		  firsthit = 0;
-		  index_of_nextrcb = 0;
+		  index_of_nextrcb = i;
 	  }
 	  else if (rr[i].is_closest == 1 && rr[i].rcb.arrival_timestamp < nextrcb.arrival_timestamp)
 	  {
 		  nextrcb = rr[i].rcb;
 		  index_of_nextrcb = i;
 	  }
-	  else
-	      index_of_nextrcb = 0;
-	  
+          
   }
 
 
   // we know which rcb will be returned and its position in the array. remove it from the array
-  for (int i = index_of_nextrcb - 1; i < *queue_cnt - 1; i ++)
+  for (int i = index_of_nextrcb; i < *queue_cnt - 1; i ++)
     {
       request_queue[i] = request_queue[i+1];
     }
@@ -165,70 +163,74 @@ struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX],int *q
 
 struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX],int  *queue_cnt, int current_cylinder, int scan_direction)
 {
-  struct REL_REQ { // struct to track rcbs in queue and their relative closeness to the current cylinder
-	  struct RCB rcb; // an RCB in the queue
-	  int dist_cylinder; // distance from the requested cylinder to the current cylinder
-	  int is_closest; // marked as one of the rcb's requesting the next closest cylinder
-  };
-  struct REL_REQ rr[*queue_cnt]; 
-
-  int cr; // closest request made by any rcb
-  // calculates the distance to the next cylinder for each rcb
-  // marks the shortest distance for use later
-  for (int i = 0; i < *queue_cnt; i++)
-  {
-	  rr[i].rcb = request_queue[i];
-	  // This is kinda hackey. the online compiler does not include math.h. 
-	  // so this will effectively be distance squared, but it shouldnt matter 
-	  // larger numbers mean farther away, smaller means closer. 
-	  rr[i].dist_cylinder = (request_queue[i].cylinder - current_cylinder) * (request_queue[i].cylinder - current_cylinder);
-	  if (i = 0)
-		  cr = rr[i].dist_cylinder;
-	  else if (cr > rr[i].dist_cylinder)
-		  cr = rr[i].dist_cylinder;
-
-  }
-
-  // marks all rcb's which are requesting the next closest rcb
-  // the earliest arriving of these will be the next chosen
-  for (int i = 0; i < *queue_cnt; i++)
-  {
-	  if (rr[i].dist_cylinder == cr)
-		  rr[i].is_closest = 1;
-  }
-
-
-  // of the rcb's marked as requesting closest, determine which arrived first
-  int firsthit = 1;
-  struct RCB nextrcb;
-  int index_of_nextrcb;
-  for (int i = 0; i < *queue_cnt; i++)
-  {
-	  if (firsthit == 1 && rr[i].is_closest == 1)
-	  {
-		  nextrcb = rr[i].rcb;
-		  firsthit = 0;
-		  index_of_nextrcb = 0;
-	  }
-	  else if (rr[i].is_closest == 1 && rr[i].rcb.arrival_timestamp < nextrcb.arrival_timestamp)
-	  {
-		  nextrcb = rr[i].rcb;
-		  index_of_nextrcb = i;
-	  }
-	  
-  }
-
-
-  // we know which rcb will be returned and its position in the array. remove it from the array
-  for (int i = index_of_nextrcb - 1; i < *queue_cnt - 1; i ++)
+    if (*queue_cnt == 0)
+        return NULLRCB;
+    
+    
+    struct REL_REQ { // struct to track rcbs in queue and their relative closeness to the current cylinder
+        struct RCB rcb; // an RCB in the queue
+        int dist_cylinder; // distance from the requested cylinder to the current cylinder
+        int is_closest; // marked as one of the rcb's requesting the next closest cylinder
+    };
+    struct REL_REQ rr[*queue_cnt];
+    
+    int cr; // closest request made by any rcb
+    // calculates the distance to the next cylinder for each rcb
+    // marks the shortest distance for use later
+    for (int i = 0; i < *queue_cnt; i++)
     {
-      request_queue[i] = request_queue[i+1];
+        rr[i].rcb = request_queue[i];
+        rr[i].is_closest = 0;
+        // This is kinda hackey. the online compiler does not include math.h.
+        // so this will effectively be distance squared, but it shouldnt matter
+        // larger numbers mean farther away, smaller means closer.
+        rr[i].dist_cylinder = (request_queue[i].cylinder - current_cylinder) * (request_queue[i].cylinder - current_cylinder);
+        if (i == 0)
+            cr = rr[i].dist_cylinder;
+        else if (cr > rr[i].dist_cylinder)
+            cr = rr[i].dist_cylinder;
+        
     }
-  *queue_cnt = *queue_cnt - 1;
-  return nextrcb;
+    
+    // marks all rcb's which are requesting the next closest rcb
+    // the earliest arriving of these will be the next chosen
+    for (int i = 0; i < *queue_cnt; i++)
+    {
+        if (rr[i].dist_cylinder == cr)
+            rr[i].is_closest = 1;
+    }
+    
+    
+    // of the rcb's marked as requesting closest, determine which arrived first
+    int firsthit = 1;
+    struct RCB nextrcb;
+    int index_of_nextrcb;
+    for (int i = 0; i < *queue_cnt; i++)
+    {
+        if (firsthit == 1 && rr[i].is_closest == 1)
+        {
+            nextrcb = rr[i].rcb;
+            firsthit = 0;
+            index_of_nextrcb = i;
+        }
+        else if (rr[i].is_closest == 1 && rr[i].rcb.arrival_timestamp < nextrcb.arrival_timestamp)
+        {
+            nextrcb = rr[i].rcb;
+            index_of_nextrcb = i;
+        }
+        
+    }
+    
+    
+    // we know which rcb will be returned and its position in the array. remove it from the array
+    for (int i = index_of_nextrcb; i < *queue_cnt - 1; i ++)
+    {
+        request_queue[i] = request_queue[i+1];
+    }
+    *queue_cnt = *queue_cnt - 1;
+    return nextrcb;
 }
-
-
+/*
 int main (int argc, char *argvp[])
 {
     struct RCB first = {1, 72, 45, 45, 1};
@@ -246,3 +248,4 @@ int main (int argc, char *argvp[])
     return 0;
 
 }
+*/
