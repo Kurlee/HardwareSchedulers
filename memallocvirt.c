c0500fccfb06a7feb3caf338b2eb0aadc4ce7792
Adrian Hassan Abdala <aha392@nyu.edu>
2019-07-13 18:28:38 +0000

    continuing work on assignment

--- memallocvirt.c
+++ memallocvirt.c
@@ -29,28 +29,25 @@ int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int
 
   else 
   {
-      struct PTE EAV; // earliest arriving valid pte
-      int EAVI; // earliest arriving valid pte index
+      int eavi; // earliest arriving valid pte index
       int first_pass; // marker for first pass through loop
       for (int i = 0; i < *table_cnt; i++)
       {
           if (page_table[i].is_valid && first_pass)
           {
-              EAV = page_table[i];
-              EAVI = i;
+              eavi = i;
               first_pass = 0;
           }
-          else if (page_table[i].is_valid && page_table[i].arrival_timestamp < EAV.arrival_timestamp)
+          else if (page_table[i].is_valid && page_table[i].arrival_timestamp < eav.arrival_timestamp)
           {
-              EAV = page_table[i];
-              EAVI = i;
+              eavi = i;
           }
       }
-      page_table[EAVI].is_valid = 0;
-      page_table[EAVI].arrival_timestamp = 0;
-      page_table[EAVI].last_access_timestamp = 0;
-      page_table[EAVI].reference_count = 0;
-      page_table[page_number].frame_numner = page_table[page_number].frame_number;
+      page_table[eavi].is_valid = 0;
+      page_table[eavi].arrival_timestamp = 0;
+      page_table[eavi].last_access_timestamp = 0;
+      page_table[eavi].reference_count = 0;
+      page_table[page_number].frame_numner = page_table[eavi].frame_number;
       return page_table[page_number].frame_number;
   }
 }
@@ -58,12 +55,179 @@ int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int
 
 
 
-int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt)
+int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int reference_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt)
 {
-
+    int faults = 0;
+    int timestamp = 0;
+    for (int i = 0; i < reference_cnt; i++ )
+    {
+        int j = reference_string[i]; // This variable holds the page number being looked up
+        // page being referenced is in memory
+        if (page_table[j].is_valid)
+        {
+            page_table[j].last_access_timestamp = timestamp;// timestamp 
+            page_table[j].reference_cnt = page_table[j].reference_cnt + 1;
+            timestamp = timestamp + 1;
+        }
+        // page being referenced is NOT in memory AND there ARE empty frames
+        else if (frame_cnt > 0)
+        {
+            int next_frame = frame_pool[frame_cnt - 1];
+            frame_pool[frame_cnt - 1] = frame_pool[frame_cnt];
+            frame_cnt = frame_cnt - 1;
+            page_table[j].frame_number = next_frame;
+            page_table[j].arrival_timestamp = timestamp;
+            page_table[j].last_access_timestamp = timestamp;
+            page_table[j].reference_count = 1;
+            timestamp = timestamp + 1;
+            faults = faults + 1;
+        }
+        // page being referenced is NOT in memory AND there ARE NO empty frames
+        else
+        {
+          int eavi; // earliest arriving valid pte index
+          int first_pass; // marker for first pass through loop
+          for (int i = 0; i < *table_cnt; i++)
+          {
+              if (page_table[i].is_valid && first_pass)
+              {
+                  // REINSERT EAV
+                  eavi = i;
+                  first_pass = 0;
+              }
+              else if (page_table[i].is_valid && page_table[i].arrival_timestamp < eav.arrival_timestamp)
+              {
+                  eavi = i;
+              }
+          }
+          page_table[eavi].is_valid = 0;
+          page_table[eavi].arrival_timestamp = 0;
+          page_table[eavi].last_access_timestamp = 0;
+          page_table[eavi].reference_count = 0;
+          page_table[j].frame_numner = page_table[eavi].frame_number;
+          page_table[j].arrival_timestamp = timestamp;
+          page_table[j].last_access_timestamp = timestamp;
+          page_table[j].reference_count = 1;
+          timestamp = timestamp + 1;
+          faults = faults + 1;
+        }
+    }
+    return faults;
 }
-int process_page_access_lru(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp); 
-int count_page_faults_lru(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
+          
+
+    
+
+int process_page_access_lru(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp);
+{
+  //if the PTE at page_number is in memory, return that frame number
+  if (page_table[page_number].is_valid == 1)
+    {
+      page_table[page_number].last_access_timestamp = current_timestamp;
+      page_table[page_number].reference_count = page_table[page_number].reference_count + 1;
+      return page_table[page_number].frame_number;
+    }
+// the page being referenced is not in memory and there are free frames
+  else if (*frame_cnt > 0)
+    {
+        int next_frame = frame_pool[frame_cnt - 1];
+        frame_pool[frame_cnt - 1] = frame_pool[frame_cnt];
+        frame_cnt = frame_cnt - 1;
+        page_table[j].frame_number = next_frame;
+        page_table[page_number].frame_number = frame_pool[*frame_cnt];
+        return page_table[page_number].frame_number;
+    }
+// the page being referenced is not in memory and there are no free frames
+  else 
+  {
+      int eavi; // earliest arriving valid pte index
+      int first_pass; // marker for first pass through loop
+      for (int i = 0; i < *table_cnt; i++)
+      {
+          if (page_table[i].is_valid && first_pass)
+          {
+              eavi = i;
+              first_pass = 0;
+          }
+          else if (page_table[i].is_valid && page_table[i].last_access_timestamp < eav.last_access_timestamp)
+          {
+              eavi = i;
+          }
+      }
+      page_table[eavi].is_valid = 0;
+      page_table[eavi].arrival_timestamp = 0;
+      page_table[eavi].last_access_timestamp = 0;
+      page_table[eavi].reference_count = 0;
+      page_table[page_number].frame_numner = page_table[eavi].frame_number;
+      return page_table[page_number].frame_number;
+  }
+}
+
+
+
+
+int count_page_faults_lru(struct PTE page_table[TABLEMAX],int table_cnt, int reference_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
+{
+    int faults = 0;
+    int timestamp = 0;
+    for (int i = 0; i < reference_cnt; i++ )
+    {
+        int j = reference_string[i]; // This variable holds the page number being looked up
+        // page being referenced is in memory
+        if (page_table[j].is_valid)
+        {
+            page_table[j].last_access_timestamp = timestamp;// timestamp 
+            page_table[j].reference_cnt = page_table[j].reference_cnt + 1;
+            timestamp = timestamp + 1;
+        }
+        // page being referenced is NOT in memory AND there ARE empty frames
+        else if (frame_cnt > 0)
+        {
+            int next_frame = frame_pool[frame_cnt - 1];
+            frame_pool[frame_cnt - 1] = frame_pool[frame_cnt];
+            frame_cnt = frame_cnt - 1;
+            page_table[j].frame_number = next_frame;
+            page_table[j].arrival_timestamp = timestamp;
+            page_table[j].last_access_timestamp = timestamp;
+            page_table[j].reference_count = 1;
+            timestamp = timestamp + 1;
+            faults = faults + 1;
+        }
+        // page being referenced is NOT in memory AND there ARE NO empty frames
+        else
+        {
+          int eavi; // earliest arriving valid pte index
+          int first_pass; // marker for first pass through loop
+          for (int i = 0; i < *table_cnt; i++)
+          {
+              if (page_table[i].is_valid && first_pass)
+              {
+                  eavi = i;
+                  first_pass = 0;
+              }
+              else if (page_table[i].is_valid && page_table[i].last_access_timestamp < eav.last_access_timestamp)
+              {
+                  eavi = i;
+              }
+          }
+          page_table[eavi].is_valid = 0;
+          page_table[eavi].arrival_timestamp = 0;
+          page_table[eavi].last_access_timestamp = 0;
+          page_table[eavi].reference_count = 0;
+          page_table[j].frame_numner = page_table[eavi].frame_number;
+          page_table[j].arrival_timestamp = timestamp;
+          page_table[j].last_access_timestamp = timestamp;
+          page_table[j].reference_count = 1;
+          timestamp = timestamp + 1;
+          faults = faults + 1;
+        }
+    }
+    return faults;
+}
+          
+
+    
+
 int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp); 
-int count_page_faults_lfu(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
+int count_page_faults_lfu(struct PTE page_table[TABLEMAX],int table_cnt, int reference_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
 
