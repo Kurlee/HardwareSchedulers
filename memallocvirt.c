ee8b7e0907f9b8ba25bac744a2f73003dd56f18e
Adrian Hassan Abdala <aha392@nyu.edu>
2019-07-13 01:13:06 +0000

    finshed first function

--- memallocvirt.c
+++ memallocvirt.c
@@ -51,12 +51,19 @@ int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int
       page_table[EAVI].last_access_timestamp = 0;
       page_table[EAVI].reference_count = 0;
       page_table[page_number].frame_numner = page_table[page_number].frame_number;
+      return page_table[page_number].frame_number;
   }
+}
 
 
 
-int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
+
+int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt)
+{
+
+}
 int process_page_access_lru(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp); 
 int count_page_faults_lru(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
 int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp); 
 int count_page_faults_lfu(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
+
